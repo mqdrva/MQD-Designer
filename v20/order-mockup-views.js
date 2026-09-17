a2d34823a20ba52c62040104bc21ed699ee52c58
mqdrva <mqdrva@gmail.com>
2026-09-17 17:38:02 -0400

    Refresh guest checkout module for sandbox testing

--- v20/order-mockup-views.js
+++ v20/order-mockup-views.js
@@ -153,5 +153,5 @@
     }
     return nativeFetch(input,init);
   };
-  import('/v20/guest-checkout.js?v=20260917a').catch(error=>console.error('MQD guest checkout module failed to load',error));
+  import('/v20/guest-checkout.js?v=20260917c').catch(error=>console.error('MQD guest checkout module failed to load',error));
 })();
